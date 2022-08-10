17a4bf05ced29af962d52fbf5019f0b9ac016d84
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-08-10 19:01:52 +0100

    Implemented Navbar

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,39 @@
+import { click } from "@syncfusion/ej2-react-grids";
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext()
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true)
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+    
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked] : true})
+    }
+
+    return (
+        <StateContext.Provider
+            value={{
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
