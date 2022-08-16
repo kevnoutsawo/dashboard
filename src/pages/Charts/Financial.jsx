fe29efa4c7bd40c292be2e47da19b7cb298e9336
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-08-16 16:23:42 +0100

    Implemented Financial Chart

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, HiloSeries, Tooltip, DateTime, Zoom, Logarithmic, Crosshair } from '@syncfusion/ej2-react-charts'
+
+import { Header } from '../../components'
+import { financialChartData, FinancialPrimaryYAxis, FinancialPrimaryXAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 const Financial = () => {
+  const { currentMode } = useStateContext()
+
   return (
-    <div>Financial</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Chart" title="Apple Inc" />
+      <ChartComponent
+        id='financial-chart'
+        height='420px'
+        primaryXAxis={FinancialPrimaryXAxis}
+        primaryYAxis={FinancialPrimaryYAxis}
+        chartArea={{ border: {width: 0}}}
+        tooltip={{ enable: true, shared: true }}
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        legendSettings={{ visible: false }}
+        crosshair={{ 
+          enable: true, 
+          lineType: 'Vertical', 
+          line: {width: 0} 
+        }}
+      >
+        <Inject services={[HiloSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
+        <SeriesCollectionDirective>
+          <SeriesDirective dataSource={financialChartData} xName="x" yName='low' name='Apple Inc' type='Hilo' low='low' high='high' />
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -5,7 +5,7 @@ import { PieChart , Header } from '../../components/'
 const Pie = () => {
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category="Chart" title="Inflation Rate" />
+      <Header category="Chart" title="Browsers Market Share" />
       <div className='w-full'>
         <PieChart />
       </div>
